078166aa226efe31988325b96f869c3cbc19997d
PLkolek <mateusz.kolaczek@gmail.com>
2016-03-20 12:06:41 +0100

    Parse whole module header!

--- src/mkolaczek/elm/MyElmParser.java
+++ src/mkolaczek/elm/MyElmParser.java
@@ -32,7 +32,53 @@ public class MyElmParser implements PsiParser {
         }
         if (builder.getTokenType() == ElmTypes.BEGIN_DOC_COMMENT) {
             docComment(builder);
+            Whitespace.freshLine(builder);
         }
+        Basic.simpleMany(builder, MyElmParser::importLine);
+    }
+
+    private static boolean importLine(PsiBuilder builder) {
+        if (builder.getTokenType() != ElmTypes.IMPORT) {
+            return false;
+        }
+        Marker m = builder.mark();
+        boolean result = Basic.sequence(builder,
+                Basic.expect(ElmTypes.IMPORT),
+                Whitespace::maybeWhitespace,
+                MyElmParser::dottedCapVar
+        );
+        if (!result) {
+            consumeUntil(builder, ElmTypes.NEW_LINE);
+            m.done(ElmTypes.IMPORT_2);
+            return false;
+        }
+        Marker m2 = builder.mark();
+        boolean hasAs = Basic.sequence(builder, Whitespace::maybeWhitespace, Basic.expect(ElmTypes.AS));
+        if (hasAs) {
+            m2.drop();
+            if (!Basic.sequence(builder, Whitespace::maybeWhitespace, Basic.expect(ElmTypes.CAP_VAR))) {
+                consumeUntil(builder, ElmTypes.NEW_LINE);
+                m.done(ElmTypes.IMPORT_2);
+                return false;
+            }
+        } else {
+            m2.rollbackTo();
+        }
+        m2 = builder.mark();
+        boolean hasExposing = Basic.sequence(builder, Whitespace::maybeWhitespace, Basic.expect(ElmTypes.EXPOSING));
+        if (hasExposing) {
+            m2.drop();
+            if (!Basic.sequence(builder, Whitespace::maybeWhitespace, listing(MyElmParser::exportValue))) {
+                consumeUntil(builder, ElmTypes.NEW_LINE);
+                m.done(ElmTypes.IMPORT_2);
+                return false;
+            }
+        } else {
+            m2.rollbackTo();
+        }
+        boolean success = Whitespace.freshLine(builder);
+        m.done(ElmTypes.IMPORT_2);
+        return success;
     }
 
     private static void docComment(PsiBuilder builder) {
@@ -67,8 +113,8 @@ public class MyElmParser implements PsiParser {
         boolean success = Basic.sequence(builder,
                 Basic.expect(ElmTypes.MODULE),
                 Whitespace::maybeWhitespace,
-                this::dottedCapVar,
-                MyElmParser.listing(exportValue()),
+                MyElmParser::dottedCapVar,
+                MyElmParser.listing(MyElmParser::exportValue),
                 Whitespace::maybeWhitespace,
                 Basic.expect(ElmTypes.WHERE),
                 Whitespace::freshLine
@@ -127,13 +173,11 @@ public class MyElmParser implements PsiParser {
         };
     }
 
-    private static Parser exportValue() {
-        return (PsiBuilder builder) ->
-                Basic.simpleOr(builder,
-                        Basic.expect(ElmTypes.LOW_VAR),
-                        MyElmParser::operator,
-                        MyElmParser::typeExport
-                );
+    private static boolean exportValue(PsiBuilder builder) {
+        return Basic.simpleOr(builder,
+                Basic.expect(ElmTypes.LOW_VAR),
+                MyElmParser::operator,
+                MyElmParser::typeExport);
     }
 
     private static boolean typeExport(PsiBuilder builder) {
@@ -156,7 +200,7 @@ public class MyElmParser implements PsiParser {
         return Basic.simpleOr(builder, ElmTypes.SYM_OP, ElmTypes.COMMA_OP);
     }
 
-    private boolean dottedCapVar(@NotNull PsiBuilder builder) {
+    private static boolean dottedCapVar(@NotNull PsiBuilder builder) {
         Marker m = builder.mark();
         boolean success = Basic.sequence(builder,
                 Basic.expect(ElmTypes.CAP_VAR),
