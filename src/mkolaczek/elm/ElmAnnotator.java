44cd454d753abe2ccfefc73a9a445ba7d362549c
PLkolek <mateusz.kolaczek@gmail.com>
2016-08-06 14:01:33 +0200

    Highlight doc comments

---
+++ src/mkolaczek/elm/ElmAnnotator.java
@@ -0,0 +1,23 @@
+package mkolaczek.elm;
+
+import com.intellij.lang.annotation.Annotation;
+import com.intellij.lang.annotation.AnnotationHolder;
+import com.intellij.lang.annotation.Annotator;
+import com.intellij.psi.PsiElement;
+import mkolaczek.elm.psi.ElmDocComment;
+import mkolaczek.elm.psi.ElmMultilineComment;
+import org.jetbrains.annotations.NotNull;
+
+public class ElmAnnotator implements Annotator {
+    @Override
+    public void annotate(@NotNull final PsiElement element, @NotNull AnnotationHolder holder) {
+        if (element instanceof ElmDocComment) {
+            Annotation infoAnnotation = holder.createInfoAnnotation(element, null);
+            infoAnnotation.setTextAttributes(ElmSyntaxHighlighter.COMMENT);
+        } else if (element instanceof ElmMultilineComment) {
+            Annotation infoAnnotation = holder.createInfoAnnotation(element, null);
+            infoAnnotation.setTextAttributes(ElmSyntaxHighlighter.DOC_COMMENT);
+        }
+
+    }
+}
--- src/mkolaczek/elm/ElmSyntaxHighlighter.java
+++ src/mkolaczek/elm/ElmSyntaxHighlighter.java
@@ -3,7 +3,6 @@ package mkolaczek.elm;
 import com.google.common.collect.Sets;
 import com.intellij.lexer.Lexer;
 import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
-import com.intellij.openapi.editor.HighlighterColors;
 import com.intellij.openapi.editor.colors.TextAttributesKey;
 import com.intellij.openapi.fileTypes.SyntaxHighlighterBase;
 import com.intellij.psi.tree.IElementType;
@@ -18,22 +17,14 @@ public class ElmSyntaxHighlighter extends SyntaxHighlighterBase {
 
     private static final Set<IElementType> KEYWORDS = Sets.newHashSet(MODULE, AS, EXPOSING, IMPORT);
 
-    public static final TextAttributesKey SEPARATOR =
-            createTextAttributesKey("SIMPLE_SEPARATOR", DefaultLanguageHighlighterColors.OPERATION_SIGN);
     public static final TextAttributesKey KEY =
-            createTextAttributesKey("SIMPLE_KEY", DefaultLanguageHighlighterColors.KEYWORD);
-    public static final TextAttributesKey VALUE =
-            createTextAttributesKey("SIMPLE_VALUE", DefaultLanguageHighlighterColors.STRING);
+            createTextAttributesKey("ELM_KEY", DefaultLanguageHighlighterColors.KEYWORD);
     public static final TextAttributesKey COMMENT =
-            createTextAttributesKey("SIMPLE_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT);
-    public static final TextAttributesKey BAD_CHARACTER =
-            createTextAttributesKey("SIMPLE_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER);
+            createTextAttributesKey("ELM_MULTILINE_COMMENT", DefaultLanguageHighlighterColors.BLOCK_COMMENT);
+    public static final TextAttributesKey DOC_COMMENT =
+            createTextAttributesKey("ELM_DOC_COMMENT", DefaultLanguageHighlighterColors.DOC_COMMENT);
 
-    private static final TextAttributesKey[] BAD_CHAR_KEYS = new TextAttributesKey[]{BAD_CHARACTER};
-    private static final TextAttributesKey[] SEPARATOR_KEYS = new TextAttributesKey[]{SEPARATOR};
     private static final TextAttributesKey[] KEY_KEYS = new TextAttributesKey[]{KEY};
-    private static final TextAttributesKey[] VALUE_KEYS = new TextAttributesKey[]{VALUE};
-    private static final TextAttributesKey[] COMMENT_KEYS = new TextAttributesKey[]{COMMENT};
     private static final TextAttributesKey[] EMPTY_KEYS = new TextAttributesKey[0];
 
     @NotNull
