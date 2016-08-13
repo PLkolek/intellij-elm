902f721dd4d26463cccf319a5bacf3754fce908e
PLkolek <mateusz.kolaczek@gmail.com>
2016-08-13 15:26:36 +0200

    Insert paretheses when autocompleting exposing

--- src/mkolaczek/elm/ElmCompletionContributor.java
+++ src/mkolaczek/elm/ElmCompletionContributor.java
@@ -1,14 +1,19 @@
 package mkolaczek.elm;
 
 import com.intellij.codeInsight.completion.*;
+import com.intellij.codeInsight.completion.util.ParenthesesInsertHandler;
+import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.patterns.ElementPattern;
 import com.intellij.patterns.PsiElementPattern.Capture;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiErrorElement;
+import com.intellij.psi.PsiFile;
 import com.intellij.psi.tree.IElementType;
 import com.intellij.util.ProcessingContext;
+import mkolaczek.elm.psi.ElmTypes;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import static com.intellij.patterns.PlatformPatterns.psiElement;
 import static com.intellij.patterns.StandardPatterns.or;
@@ -39,7 +44,27 @@ public class ElmCompletionContributor extends CompletionContributor {
                                                ProcessingContext context,
                                                @NotNull CompletionResultSet resultSet) {
                         resultSet.addElement(LookupElementBuilder.create("as"));
-                        resultSet.addElement(LookupElementBuilder.create("exposing"));
+                        resultSet.addElement(LookupElementBuilder.create("exposing").withInsertHandler(new ParenthesesInsertHandler<LookupElement>(true, false, true, true) {
+                            @Override
+                            protected boolean placeCaretInsideParentheses(InsertionContext context, LookupElement item) {
+                                return true;
+                            }
+
+                            @Nullable
+                            @Override
+                            protected PsiElement findNextToken(InsertionContext context) {
+                                final PsiFile file = context.getFile();
+                                PsiElement element = file.findElementAt(context.getTailOffset());
+                                if (element != null && isWhitespace(element)) {
+                                    element = file.findElementAt(element.getTextRange().getEndOffset());
+                                }
+                                return element;
+                            }
+
+                            private boolean isWhitespace(PsiElement element) {
+                                return element.getNode().getElementType() == ElmTypes.WHITE_SPACE || element.getNode().getElementType() == ElmTypes.NEW_LINE;
+                            }
+                        }));
                     }
                 }
         );
