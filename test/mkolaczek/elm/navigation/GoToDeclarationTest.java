742231340cebb88b8125d221fa14846958dd389c
PLkolek <mateusz.kolaczek@gmail.com>
2016-09-24 16:37:56 +0200

    Obsolete comments

--- test/mkolaczek/elm/navigation/GoToDeclarationTest.java
+++ test/mkolaczek/elm/navigation/GoToDeclarationTest.java
@@ -18,14 +18,7 @@ public class GoToDeclarationTest extends LightCodeInsightFixtureTestCase {
         //which prevents GoToDeclarationAction from opening new editor, so here I do it manually
         Editor oldEditor = getEditor();
         myFixture.openFileInEditor(files[1].getVirtualFile());
-        //perform go to by hand (see com.intellij.codeInsight.navigation.GotoDeclarationTest
         new GotoDeclarationAction().invoke(getProject(), oldEditor, files[0]);
-        /*PsiElement element = GotoDeclarationAction.findTargetElement(getProject(), getEditor(), getEditor().getCaretModel().getOffset());
-        assertThat(element.getContainingFile(), is(files[1]));
-        get
-        getEditor().getCaretModel().moveToOffset(element.getTextOffset());
-        getEditor().getScrollingModel().scrollToCaret(ScrollType.CENTER);
-        getEditor().getSelectionModel().removeSelection();*/
 
         myFixture.checkResultByFile("Test2.expected.elm");
     }
