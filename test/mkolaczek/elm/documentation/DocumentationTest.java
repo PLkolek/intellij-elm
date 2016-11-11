ca3df9bf6707a5838c43475ee8fdcbc2eab6f130
PLkolek <mateusz.kolaczek@gmail.com>
2016-11-11 19:06:31 +0100

    Test for documentation provider

---
+++ test/mkolaczek/elm/documentation/DocumentationTest.java
@@ -0,0 +1,26 @@
+package mkolaczek.elm.documentation;
+
+import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
+import mkolaczek.elm.psi.ElmFile;
+import mkolaczek.elm.psi.node.ElmModule;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class DocumentationTest extends LightCodeInsightFixtureTestCase {
+
+    @Override
+    protected String getTestDataPath() {
+        return "testdata/documentation";
+    }
+
+    public void testModuleQuickNavigateInfo() {
+        myFixture.configureByFile("Test.elm");
+        ElmModule module = ((ElmFile) myFixture.getFile()).module();
+        String info = new ElmDocumentationProvider().getQuickNavigateInfo(module, null);
+        String expected = "module Test1.B exposing (aaaaaaa, bbbbbbb, aaaaaaaaa, abbjaaal, (    **              ), " +
+                "(,,), A(           ..     ), B(Abc, Def, Ghci), C(Abc, Def, Ghci, Xxxxxx, Aaaaa, Bvvvvvvvvvvvvvvvvv))";
+        assertThat(info, is(expected));
+
+    }
+}
