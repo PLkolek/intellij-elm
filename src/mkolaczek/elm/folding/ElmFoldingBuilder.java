de325f1c0d4bfc56b5aff85e32497d0e222d2197
PLkolek <mateusz.kolaczek@gmail.com>
2016-11-12 13:57:30 +0100

    Folding

--- src/mkolaczek/elm/folding/ElmFoldingBuilder.java
+++ src/mkolaczek/elm/folding/ElmFoldingBuilder.java
@@ -1,18 +1,32 @@
 package mkolaczek.elm.folding;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import com.intellij.lang.ASTNode;
 import com.intellij.lang.folding.FoldingBuilder;
 import com.intellij.lang.folding.FoldingDescriptor;
 import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.util.PsiTreeUtil;
 import mkolaczek.elm.psi.ElmElementTypes;
+import mkolaczek.elm.psi.node.ElmModuleHeader;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.Set;
+
+import static mkolaczek.elm.psi.ElmElementTypes.*;
 
 public class ElmFoldingBuilder implements FoldingBuilder {
+
+    private static final Set<IElementType> FOLDABLE_NODES = Sets.newHashSet(
+            DOC_COMMENT,
+            MULTILINE_COMMENT,
+            IMPORTS,
+            EXPOSING_NODE);
+
     @NotNull
     @Override
     public FoldingDescriptor[] buildFoldRegions(@NotNull ASTNode node, @NotNull Document document) {
@@ -22,8 +36,7 @@ public class ElmFoldingBuilder implements FoldingBuilder {
     }
 
     private void collectDescriptors(ASTNode node, Document document, List<FoldingDescriptor> descriptors) {
-        final IElementType type = node.getElementType();
-        if (type == ElmElementTypes.DOC_COMMENT || type == ElmElementTypes.MULTILINE_COMMENT) {
+        if (FOLDABLE_NODES.contains(node.getElementType()) && spanMultipleLines(node, document)) {
             descriptors.add(new FoldingDescriptor(node, node.getTextRange()));
         }
 
@@ -36,17 +49,36 @@ public class ElmFoldingBuilder implements FoldingBuilder {
     @Override
     public String getPlaceholderText(@NotNull ASTNode node) {
         final IElementType type = node.getElementType();
-        if(type == ElmElementTypes.DOC_COMMENT) {
+        if (type == DOC_COMMENT) {
             return "{-|...-}";
         }
-        if(type == ElmElementTypes.MULTILINE_COMMENT) {
+        if (type == ElmElementTypes.MULTILINE_COMMENT) {
             return "{-...-}";
+        }
+        if (type == IMPORTS) {
+            return "import ...";
+        }
+        if (type == EXPOSING_NODE) {
+            return "exposing ...";
         }
         return null;
     }
 
     @Override
     public boolean isCollapsedByDefault(@NotNull ASTNode node) {
+        IElementType type = node.getElementType();
+        if (type == IMPORTS) {
+            return true;
+        }
+        if (type == EXPOSING_NODE && PsiTreeUtil.getParentOfType(node.getPsi(), ElmModuleHeader.class) != null) {
+            return true;
+        }
+
         return false;
+    }
+
+    private static boolean spanMultipleLines(@NotNull ASTNode node, @NotNull Document document) {
+        final TextRange range = node.getTextRange();
+        return document.getLineNumber(range.getStartOffset()) < document.getLineNumber(range.getEndOffset());
     }
 }
