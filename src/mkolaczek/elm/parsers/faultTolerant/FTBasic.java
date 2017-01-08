79be2822857c286f368f45ae77f0f882e4d8423c
PLkolek <mateusz.kolaczek@gmail.com>
2017-01-08 15:12:46 +0100

    Who needs full class for ListingContent?

--- src/mkolaczek/elm/parsers/faultTolerant/FTBasic.java
+++ src/mkolaczek/elm/parsers/faultTolerant/FTBasic.java
@@ -1,8 +1,10 @@
 package mkolaczek.elm.parsers.faultTolerant;
 
 import mkolaczek.elm.psi.Elements;
+import mkolaczek.elm.psi.Tokens;
 
 import static mkolaczek.elm.parsers.faultTolerant.Expect.expect;
+import static mkolaczek.elm.parsers.faultTolerant.Expect.expectAs;
 import static mkolaczek.elm.parsers.faultTolerant.Sequence.sequenceAs;
 import static mkolaczek.elm.parsers.faultTolerant.WhiteSpace.maybeWhitespace;
 import static mkolaczek.elm.psi.Tokens.LPAREN;
@@ -13,8 +15,16 @@ public class FTBasic {
     public static FTParser listing(String name, FTParser listedValue) {
         return sequenceAs(name, Elements.MODULE_VALUE_LIST,
                 expect(LPAREN),
-                new ListingContent(name + " content", listedValue),
+                listingContent(name + " content", listedValue),
                 expect(RPAREN)
         ).separatedBy(maybeWhitespace());
     }
+
+    public static FTParser listingContent(String name, FTParser listedValue) {
+        return new Or(name,
+                expectAs(Tokens.OPEN_LISTING, Elements.OPEN_LISTING_NODE),
+                new ListingValues(listedValue)
+        );
+    }
+
 }
