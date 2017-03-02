f89e9fc5fae74d9010d12efeda531826c6c3ec3b
PLkolek <mateusz.kolaczek@gmail.com>
2017-03-02 08:57:07 +0100

    Error handling in try, resolves problems with import autocompletion

--- src/mkolaczek/elm/parsers/core/Try.java
+++ src/mkolaczek/elm/parsers/core/Try.java
@@ -1,9 +1,14 @@
 package mkolaczek.elm.parsers.core;
 
+import com.google.common.collect.Sets;
 import com.intellij.lang.PsiBuilder;
+import com.intellij.psi.tree.IElementType;
 import mkolaczek.elm.psi.Token;
 
+import java.util.Optional;
 import java.util.Set;
+
+import static mkolaczek.elm.parsers.core.SkipUntil.nextValid;
 
 public class Try extends ParserAbstr {
 
@@ -20,10 +25,20 @@ public class Try extends ParserAbstr {
 
     @Override
     protected void parse2(PsiBuilder builder, Set<Token> myNextTokens) {
+        Set<Token> nextTokens = Sets.union(myNextTokens, startingTokens());
         //noinspection SuspiciousMethodCalls
-        if (contents.startingTokens().contains(builder.getTokenType())) {
+        IElementType token = builder.getTokenType();
+        //noinspection SuspiciousMethodCalls
+        if (startingTokens().contains(token)) {
             contents.parse(builder, myNextTokens);
-        }
+        } else //noinspection SuspiciousMethodCalls
+            if (!myNextTokens.contains(token)) {
+                Optional<Token> nextValid = nextValid(nextTokens, builder);
+                if (nextValid.isPresent() && startingTokens().contains(nextValid.get())) {
+                    SkipUntil.skipUntil(name(), startingTokens(), builder);
+                    contents.parse(builder, myNextTokens);
+                }
+            }
     }
 
     @Override
