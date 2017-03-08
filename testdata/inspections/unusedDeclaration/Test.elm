4c91789f78dec96f21a3ca3cfca0ae9231a8f07b
PLkolek <mateusz.kolaczek@gmail.com>
2017-03-08 11:23:25 +0100

    Unused declaration for type constructors test

--- testdata/inspections/unusedDeclaration/Test.elm
+++ testdata/inspections/unusedDeclaration/Test.elm
@@ -1,4 +1,4 @@
 module <warning descr="Unused module"><caret>Test</warning> exposing (..)
 import Test2
 
-type alias <warning descr="Unused type">X</warning> = A1
+type <warning descr="Unused type">X</warning> = <warning descr="Unused type constructor">A1</warning>
