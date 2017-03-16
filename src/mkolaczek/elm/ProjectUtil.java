51c3675066ebfcba45fc4fcab654079e39ce80d9
PLkolek <mateusz.kolaczek@gmail.com>
2017-03-16 11:15:35 +0100

    Exlude test files from packages

--- src/mkolaczek/elm/ProjectUtil.java
+++ src/mkolaczek/elm/ProjectUtil.java
@@ -11,14 +11,12 @@ import mkolaczek.elm.psi.ElmFile;
 import mkolaczek.elm.psi.node.Module;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collection;
 import java.util.Objects;
 import java.util.stream.Stream;
 
 public class ProjectUtil {
     public static Stream<Module> modules(Project project) {
-        return elmFiles(project).stream()
-                                .map(PsiManager.getInstance(project)::findFile)
+        return elmFiles(project).map(PsiManager.getInstance(project)::findFile)
                                 .map(ElmFile.class::cast)
                                 .filter(Objects::nonNull)
                                 .map(ElmFile::module)
@@ -26,9 +24,15 @@ public class ProjectUtil {
     }
 
     @NotNull
-    public static Collection<VirtualFile> elmFiles(Project project) {
+    public static Stream<VirtualFile> elmFiles(Project project) {
         GlobalSearchScope scope = GlobalSearchScope.allScope(project);
-        return FileBasedIndex.getInstance().getContainingFiles(FileTypeIndex.NAME, ElmFileType.INSTANCE, scope);
+        return FileBasedIndex.getInstance().getContainingFiles(FileTypeIndex.NAME, ElmFileType.INSTANCE, scope)
+                             .stream()
+                             .filter(f -> !isPackageTestFile(f));
+    }
+
+    private static boolean isPackageTestFile(VirtualFile f) {
+        return f.getPath().contains("/elm-stuff/") && f.getPath().contains("/tests/");
     }
 
     public static Stream<Module> modules(Project project, String searchedModuleName) {
