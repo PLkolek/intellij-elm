6ee772d77db8448e45f8e93ebce530bfc1d79db3
PLkolek <mateusz.kolaczek@gmail.com>
2017-03-17 12:04:22 +0100

    Test for unused operator

--- testdata/features/inspections/unusedDeclaration/Test.elm
+++ testdata/features/inspections/unusedDeclaration/Test.elm
@@ -1,3 +1,5 @@
 module <warning descr="Unused module"><caret>Test</warning> exposing (..)
 
 type <warning descr="Unused type">X</warning> = <warning descr="Unused type constructor">A1</warning>
+
+infix 7 <warning descr="Unused operator">.-></warning>
