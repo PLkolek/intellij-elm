241fa75975345fa2f2b841fb0fecdb87f23d70c3
PLkolek <mateusz.kolaczek@gmail.com>
2017-03-19 14:29:25 +0100

    Coloring of strings

--- src/mkolaczek/elm/features/syntaxHinglighting/ElmSyntaxHighlighter.java
+++ src/mkolaczek/elm/features/syntaxHinglighting/ElmSyntaxHighlighter.java
@@ -1,5 +1,6 @@
 package mkolaczek.elm.features.syntaxHinglighting;
 
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
 import com.intellij.lexer.Lexer;
 import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
@@ -29,7 +30,13 @@ public class ElmSyntaxHighlighter extends SyntaxHighlighterBase {
             Tokens.INFIXL,
             Tokens.INFIXR,
             Tokens.INFIX
-            );
+    );
+
+    private static final Set<IElementType> STRING_LITERALS = ImmutableSet.of(
+            Tokens.MULTILINE_STRING,
+            Tokens.QUOTE,
+            Tokens.STRING_CONTENT
+    );
 
     public static final TextAttributesKey KEY =
             createTextAttributesKey("ELM_KEY", DefaultLanguageHighlighterColors.KEYWORD);
@@ -39,9 +46,12 @@ public class ElmSyntaxHighlighter extends SyntaxHighlighterBase {
             createTextAttributesKey("ELM_LINE_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT);
     public static final TextAttributesKey DOC_COMMENT =
             createTextAttributesKey("ELM_DOC_COMMENT", DefaultLanguageHighlighterColors.DOC_COMMENT);
+    public static final TextAttributesKey STRING =
+            createTextAttributesKey("ELM_STRING", DefaultLanguageHighlighterColors.STRING);
 
     private static final TextAttributesKey[] KEY_KEYS = new TextAttributesKey[]{KEY};
     private static final TextAttributesKey[] COMMENT_KEYS = new TextAttributesKey[]{LINE_COMMENT};
+    private static final TextAttributesKey[] STRING_KEYS = new TextAttributesKey[]{STRING};
     private static final TextAttributesKey[] EMPTY_KEYS = new TextAttributesKey[0];
 
     @NotNull
@@ -58,6 +68,9 @@ public class ElmSyntaxHighlighter extends SyntaxHighlighterBase {
         }
         if (Tokens.COMMENT_TOKENS.contains(tokenType)) {
             return COMMENT_KEYS;
+        }
+        if (STRING_LITERALS.contains(tokenType)) {
+            return STRING_KEYS;
         }
         return EMPTY_KEYS;
     }
