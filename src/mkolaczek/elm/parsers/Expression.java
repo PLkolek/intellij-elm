0ce11f9368a6a14965ead574f99d34433b17e935
PLkolek <mateusz.kolaczek@gmail.com>
2017-03-21 11:14:23 +0100

    Maybe it seems to work

--- src/mkolaczek/elm/parsers/Expression.java
+++ src/mkolaczek/elm/parsers/Expression.java
@@ -1,13 +1,17 @@
 package mkolaczek.elm.parsers;
 
 import mkolaczek.elm.parsers.core.Parser;
+import mkolaczek.elm.parsers.core.ParserBox;
 import mkolaczek.elm.psi.Tokens;
 
 import static com.google.common.collect.Sets.newHashSet;
 import static mkolaczek.elm.parsers.Basic.spacePrefix;
 import static mkolaczek.elm.parsers.core.Expect.expect;
+import static mkolaczek.elm.parsers.core.IndentedBlock.indentedBlock;
+import static mkolaczek.elm.parsers.core.Many.many;
 import static mkolaczek.elm.parsers.core.Or.or;
 import static mkolaczek.elm.parsers.core.Sequence.sequence;
+import static mkolaczek.elm.parsers.core.WhiteSpace.indented;
 import static mkolaczek.elm.parsers.core.WhiteSpace.maybeWhitespace;
 import static mkolaczek.elm.psi.Tokens.*;
 
@@ -44,21 +48,34 @@ public class Expression {
         return sequence(
                 spacePrefix(Pattern.term()),
                 maybeWhitespace(expect(EQUALS)),
-                expression()
+                expression
         );
     }
 
-    private static Parser expression() {
+    private static final ParserBox expression = new ParserBox("expression");
+
+    static {
         //TODO: implement
-        return or(
-                let()
+        expression.setParser(
+                or(
+                        let(),
+
+                        //TODO: just for testing
+                        expect(CAP_VAR)
+                )
         );
     }
 
     private static Parser let() {
         //TODO: implement
         return sequence(
-                maybeWhitespace(expect(Tokens.LET))
+                maybeWhitespace(expect(Tokens.LET)),
+                indentedBlock(
+                        sequence(
+                                definition(),
+                                many(indented(definition()))
+                        )
+                )
         );
     }
 }
