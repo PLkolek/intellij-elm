af32628b270ea42e2529ab1abaf365a75a7e93a5
PLkolek <mateusz.kolaczek@gmail.com>
2017-03-27 18:52:23 +0200

    Fixed unused test

--- src/mkolaczek/elm/psi/node/InfixDeclaration.java
+++ src/mkolaczek/elm/psi/node/InfixDeclaration.java
@@ -2,12 +2,10 @@ package mkolaczek.elm.psi.node;
 
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiReference;
 import com.intellij.psi.util.PsiTreeUtil;
 import mkolaczek.elm.ElmElementFactory;
 import mkolaczek.elm.psi.node.extensions.Declaration;
 import mkolaczek.elm.psi.node.extensions.ElmNamedElement;
-import mkolaczek.elm.references.ValueReference;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -28,10 +26,5 @@ public class InfixDeclaration extends ElmNamedElement implements Declaration {
     public PsiElement getNameIdentifier() {
         //required for ctrl+click find usages to work
         return PsiTreeUtil.findChildOfType(this, OperatorSymbolRef.class);
-    }
-
-    @Override
-    public PsiReference getReference() {
-        return new ValueReference(this);
     }
 }
