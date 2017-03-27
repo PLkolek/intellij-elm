a543a26c2ecdac3d4f36eb629fdfc0a624968879
PLkolek <mateusz.kolaczek@gmail.com>
2017-03-27 12:38:19 +0200

    More specific test for unused declaration

--- testdata/features/inspections/unusedDeclaration/Test.elm
+++ testdata/features/inspections/unusedDeclaration/Test.elm
@@ -2,8 +2,15 @@ module <warning descr="Unused module"><caret>Test</warning> exposing (..)
 
 type <warning descr="Unused type">X</warning> = <warning descr="Unused type constructor">A1</warning>
 
-type PortType = <warning descr="Unused type constructor">Cons</warning>
+type PortType = <warning descr="Unused type constructor">Cons</warning> | UsedCons
 
 infix 7 <warning descr="Unused operator">.-></warning>
 
 port <warning descr="Unused port">somePort</warning> : PortType
+
+port usedPort : () -> PortType
+
+useThings =
+    ( usedPort ()
+    , UsedCons
+    )
