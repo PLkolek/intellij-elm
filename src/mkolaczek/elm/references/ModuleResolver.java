4767b87031fff07e3c9450db869708e8b33f5e73
PLkolek <mateusz.kolaczek@gmail.com>
2017-03-29 12:07:04 +0200

    Cleaned module resolver a bit

--- src/mkolaczek/elm/references/ModuleResolver.java
+++ src/mkolaczek/elm/references/ModuleResolver.java
@@ -20,51 +20,33 @@ import static mkolaczek.elm.psi.node.Module.module;
 
 public class ModuleResolver {
 
-    public static Stream<PsiElement> resolveToAlias(ModuleNameRef target) {
-        Optional<Stream<Module>> inImport = inImport(target);
-        if (inImport.isPresent()) {
-            return inImport.get().filter(m -> m.sameName(target.getName())).map(x -> x);
-        }
-        return Stream.of(
-                module(target)
-                        .notAliasedImports()
-                        .filter(i -> target.getName().equals(i.importedModuleNameString().orElse(null)))
-                        .flatMap(import_ -> ProjectUtil.modules(import_.getProject(),
-                                import_.importedModuleNameString().orElse(null))),
-
-                module(target)
-                        .aliasedImports()
-                        .map(Import::alias)
-                        .flatMap(Streams::stream)
-                        .filter(a -> a.sameName(target.getName())),
-
-                BuiltInImports.imports()
-                              .filter(i -> i.importedAs().equals(target.getName()))
-                              .flatMap(i -> ProjectUtil.modules(target.getProject(), i.moduleName()))
-        ).flatMap(Function.identity());
+    public static Stream<? extends PsiElement> resolveToAlias(ModuleNameRef target) {
+        return resolve(target, aliased -> aliased.map(Import::alias)
+                                                 .flatMap(Streams::stream)
+                                                 .filter(a -> a.sameName(target.getName()))
+                                                 .map(x -> x));
     }
 
     public static Stream<Module> resolveToModule(ModuleNameRef target) {
+        return resolve(target,
+                aliased -> aliased.filter(i -> i.importedAs(target.getName()))
+                                  .flatMap(import_ -> {
+                                      String moduleName = import_.importedModuleNameString().orElse(null);
+                                      return ProjectUtil.modules(import_.getProject(), moduleName);
+                                  })
+        ).map(m -> (Module) m);
+    }
+
+    private static Stream<? extends PsiElement> resolve(ModuleNameRef target,
+                                                        Function<Stream<Import>, Stream<PsiElement>> aliasedTransform) {
         Optional<Stream<Module>> inImport = inImport(target);
         if (inImport.isPresent()) {
             return inImport.get().filter(m -> m.sameName(target.getName()));
         }
         return Stream.of(
-                module(target)
-                        .notAliasedImports()
-                        .filter(i -> target.getName().equals(i.importedModuleNameString().orElse(null)))
-                        .flatMap(import_ -> ProjectUtil.modules(import_.getProject(),
-                                import_.importedModuleNameString().orElse(null))),
-
-                module(target)
-                        .aliasedImports()
-                        .filter(i -> i.importedAs(target.getName()))
-                        .flatMap(import_ -> ProjectUtil.modules(import_.getProject(),
-                                import_.importedModuleNameString().orElse(null))),
-
-                BuiltInImports.imports()
-                              .filter(i -> i.importedAs().equals(target.getName()))
-                              .flatMap(i -> ProjectUtil.modules(target.getProject(), i.moduleName()))
+                sameNameNotAliased(target),
+                aliasedTransform.apply(aliasedImports(target)),
+                sameNameBuiltIn(target)
         ).flatMap(Function.identity());
     }
 
@@ -86,19 +68,41 @@ public class ModuleResolver {
                                                         .collect(toSet());
                     alreadyImported.addAll(BuiltInImports.moduleNames().collect(toSet()));
                     return Stream.of(
-                            module.notAliasedImports()
-                                  .map(Import::importedModuleNameString)
-                                  .flatMap(Streams::stream),
-                            module.aliasedImports()
-                                  .map(Import::aliasName)
-                                  .flatMap(Streams::stream),
-                            BuiltInImports.imports()
-                                          .map(BuiltInImport::importedAs),
-                            ProjectUtil.modules(target.getProject()).filter(m -> !alreadyImported.contains(m.getName()))
+                            notAliasedImports(target).map(Import::importedModuleNameString)
+                                                     .flatMap(Streams::stream),
+
+                            aliasedImports(target).map(Import::aliasName)
+                                                  .flatMap(Streams::stream),
+
+                            BuiltInImports.imports().map(BuiltInImport::importedAs),
+
+                            ProjectUtil.modules(target.getProject())
+                                       .filter(m -> !alreadyImported.contains(m.getName()))
                                        .map(Module::getName)
                     ).flatMap(Function.identity());
                 }
         ).filter(name -> !module.sameName(name));
+    }
+
+    private static Stream<Import> notAliasedImports(PsiElement target) {
+        return module(target).notAliasedImports();
+    }
+
+    private static Stream<Module> sameNameBuiltIn(ModuleNameRef target) {
+        return BuiltInImports.imports()
+                             .filter(i -> i.importedAs().equals(target.getName()))
+                             .flatMap(i -> ProjectUtil.modules(target.getProject(), i.moduleName()));
+    }
+
+    private static Stream<Module> sameNameNotAliased(ModuleNameRef target) {
+        return notAliasedImports(target)
+                .filter(i -> target.getName().equals(i.importedModuleNameString().orElse(null)))
+                .flatMap(import_ -> ProjectUtil.modules(import_.getProject(),
+                        import_.importedModuleNameString().orElse(null)));
+    }
+
+    public static Stream<Import> aliasedImports(PsiElement target) {
+        return module(target).aliasedImports();
     }
 
     private static Optional<Stream<Module>> inImport(PsiElement target) {
