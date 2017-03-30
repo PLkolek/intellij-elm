350f6de6a5d45d22780b3aca1a060d58710ed403
PLkolek <mateusz.kolaczek@gmail.com>
2017-03-30 08:12:01 +0200

    Highlighting of punctuation

--- src/mkolaczek/elm/features/syntaxHinglighting/ElmSyntaxHighlighter.java
+++ src/mkolaczek/elm/features/syntaxHinglighting/ElmSyntaxHighlighter.java
@@ -1,23 +1,23 @@
 package mkolaczek.elm.features.syntaxHinglighting;
 
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Sets;
+import com.google.common.collect.Maps;
 import com.intellij.lexer.Lexer;
 import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
 import com.intellij.openapi.editor.colors.TextAttributesKey;
 import com.intellij.openapi.fileTypes.SyntaxHighlighterBase;
 import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.tree.TokenSet;
 import mkolaczek.elm.lexer.ElmLexerAdapter;
 import mkolaczek.elm.psi.Tokens;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Set;
+import java.util.Map;
 
 import static com.intellij.openapi.editor.colors.TextAttributesKey.createTextAttributesKey;
 
 class ElmSyntaxHighlighter extends SyntaxHighlighterBase {
 
-    private static final Set<IElementType> KEYWORDS = Sets.newHashSet(
+    private static final TokenSet KEYWORDS = TokenSet.create(
             Tokens.MODULE,
             Tokens.AS,
             Tokens.EXPOSING,
@@ -39,31 +39,58 @@ class ElmSyntaxHighlighter extends SyntaxHighlighterBase {
             Tokens.OF
     );
 
-    private static final Set<IElementType> VALID_ESCAPES = ImmutableSet.of(
-            Tokens.VALID_STRING_ESCAPE_TOKEN
-    );
-
-    private static final Set<IElementType> INVALID_ESCAPES = ImmutableSet.of(
+    private static final TokenSet INVALID_ESCAPES = TokenSet.create(
             Tokens.INVALID_CHARACTER_ESCAPE_TOKEN,
             Tokens.INVALID_UNICODE_ESCAPE_TOKEN
     );
 
-    private static final Set<IElementType> STRING_LITERALS = ImmutableSet.of(
+    private static final TokenSet STRING_LITERALS = TokenSet.create(
             Tokens.MULTILINE_STRING,
             Tokens.QUOTE,
             Tokens.STRING_CONTENT
     );
 
-    private static final Set<IElementType> GLSL_TOKENS = ImmutableSet.of(
-            Tokens.BEGIN_GLSL,
+    private static final TokenSet GLSL_TOKENS = TokenSet.create(Tokens.BEGIN_GLSL,
             Tokens.GLSL_CONTENT,
             Tokens.END_GLSL
     );
 
+    private static final TokenSet PARENTHESES = TokenSet.create(Tokens.LPAREN, Tokens.RPAREN);
+    private static final TokenSet BRACES = TokenSet.create(Tokens.LBRACKET, Tokens.RBRACKET);
+    private static final TokenSet BRACKETS = TokenSet.create(Tokens.LSQUAREBRACKET, Tokens.RBRACKET);
 
-    private static final TextAttributesKey[] EMPTY_KEYS = new TextAttributesKey[0];
-    public static final TextAttributesKey ELM_ARROW =
+    private static final TextAttributesKey ELM_ARROW =
             createTextAttributesKey("ELM_ARROW", DefaultLanguageHighlighterColors.OPERATION_SIGN);
+    private static final TextAttributesKey ELM_PARENTHESIS =
+            createTextAttributesKey("ELM_PARENTHESIS", DefaultLanguageHighlighterColors.PARENTHESES);
+    private static final TextAttributesKey ELM_BRACES =
+            createTextAttributesKey("ELM_BRACES", DefaultLanguageHighlighterColors.BRACES);
+    private static final TextAttributesKey ELM_BRACKETS =
+            createTextAttributesKey("ELM_BRACKETS", DefaultLanguageHighlighterColors.BRACKETS);
+    private static final TextAttributesKey ELM_COMMA =
+            createTextAttributesKey("ELM_COMMA", DefaultLanguageHighlighterColors.COMMA);
+    private static final TextAttributesKey ELM_EQ =
+            createTextAttributesKey("ELM_EQ", DefaultLanguageHighlighterColors.OPERATION_SIGN);
+    private static final TextAttributesKey ELM_PIPE =
+            createTextAttributesKey("ELM_PIPE", DefaultLanguageHighlighterColors.OPERATION_SIGN);
+
+    private static final Map<IElementType, TextAttributesKey> coloring = Maps.newHashMap();
+
+    static {
+        safeMap(coloring, KEYWORDS, DefaultLanguageHighlighterColors.KEYWORD);
+        safeMap(coloring, STRING_LITERALS, DefaultLanguageHighlighterColors.STRING);
+        safeMap(coloring, INVALID_ESCAPES, DefaultLanguageHighlighterColors.INVALID_STRING_ESCAPE);
+        safeMap(coloring, GLSL_TOKENS, DefaultLanguageHighlighterColors.STRING);
+        safeMap(coloring, PARENTHESES, ELM_PARENTHESIS);
+        safeMap(coloring, BRACES, ELM_BRACES);
+        safeMap(coloring, BRACKETS, ELM_BRACKETS);
+        safeMap(coloring, Tokens.COMMENT_TOKENS, DefaultLanguageHighlighterColors.LINE_COMMENT);
+        safeMap(coloring, Tokens.VALID_STRING_ESCAPE_TOKEN, DefaultLanguageHighlighterColors.VALID_STRING_ESCAPE);
+        safeMap(coloring, Tokens.ARROW, ELM_ARROW);
+        safeMap(coloring, Tokens.EQUALS, ELM_EQ);
+        safeMap(coloring, Tokens.PIPE, ELM_PIPE);
+        safeMap(coloring, Tokens.COMMA, ELM_COMMA);
+    }
 
     @NotNull
     @Override
@@ -74,28 +101,7 @@ class ElmSyntaxHighlighter extends SyntaxHighlighterBase {
     @NotNull
     @Override
     public TextAttributesKey[] getTokenHighlights(IElementType tokenType) {
-        if (KEYWORDS.contains(tokenType)) {
-            return pack(DefaultLanguageHighlighterColors.KEYWORD);
-        }
-        if (Tokens.COMMENT_TOKENS.contains(tokenType)) {
-            return pack(DefaultLanguageHighlighterColors.LINE_COMMENT);
-        }
-        if (STRING_LITERALS.contains(tokenType)) {
-            return pack(DefaultLanguageHighlighterColors.STRING);
-        }
-        if (VALID_ESCAPES.contains(tokenType)) {
-            return pack(DefaultLanguageHighlighterColors.VALID_STRING_ESCAPE);
-        }
-        if (INVALID_ESCAPES.contains(tokenType)) {
-            return pack(DefaultLanguageHighlighterColors.INVALID_STRING_ESCAPE);
-        }
-        if (GLSL_TOKENS.contains(tokenType)) {
-            return pack(DefaultLanguageHighlighterColors.STRING);
-        }
-        if (tokenType == Tokens.ARROW) {
-            return pack(ELM_ARROW);
-        }
-        return EMPTY_KEYS;
+        return pack(coloring.get(tokenType));
     }
 
 }
